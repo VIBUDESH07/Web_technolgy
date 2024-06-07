692429695042219ae6b00160e74b45904296e877
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-07 08:54:23 +0530

    Update Header.jsx

--- web_tech/src/Components/Header.jsx
+++ web_tech/src/Components/Header.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome, FaSearch } from 'react-icons/fa'; // Importing home and search icons
+import '../Styles/Header.css'; // Ensure this path is correct
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">
+          <FaHome />
+        </Link>
+      </div>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search..." />
+        <Link to="/search">
+        <FaSearch className="search-icon" />
+        </Link>
+      </div>
+      <div className="navbar-links">
+       
+        <Link to="/login">
+          <button className="login-button">Login</button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
