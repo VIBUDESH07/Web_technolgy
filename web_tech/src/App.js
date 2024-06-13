819e840b468f2ef53d58f29ec8c24036b9db7e05
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-06-13 19:44:48 +0530

    Update App.js

--- web_tech/src/App.js
+++ web_tech/src/App.js
@@ -1,24 +1,26 @@
-import {Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Components/Login';
-import Header from './Components/Header' 
+import Header from './Components/Header'; 
 import Update from './Components/Update';
 import Form from './Components/Form';
 import SuperAdminLogin from './Components/SuperAdminLogin';
 import SuperAdminDashboard from './Components/SuperAdminDashboard';
+import Updatedata from './Components/Updatedata';
+
 function App() {
   return (
     <div>
       <Routes>
-        
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/" element={<Header/>}/>
-        <Route path="/update" element={<Update/>}/>
-        <Route path="/add" element={<Form/>}/>
-        <Route path="/s" element={<SuperAdminLogin/>} />
-        <Route path="/super" element={<SuperAdminDashboard/>} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Header />} />
+        <Route path="/update" element={<Update />} />
+        <Route path="/add" element={<Form />} />
+        <Route path="/superadmin/login" element={<SuperAdminLogin />} />
+        <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
+        <Route path="/update/:id" element={<Updatedata />} />
       </Routes>
-    </div>  );
+    </div>
+  );
 }
 
 export default App;
