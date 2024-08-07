8acbcb9ee47be2e17a3d5f60aeb55d502c7a01e4
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-08-07 13:25:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 const schedule = require('node-schedule');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const mongoURI = 'mongodb+srv://vibudesh:040705@cluster0.oug8gz8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true'
 app.use(bodyParser.json());
 app.use(cors({
